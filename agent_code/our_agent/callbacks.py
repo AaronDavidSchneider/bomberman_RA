a70cf70d75b76aefdcd709b58671dcba61f269ee
Aaron <Aaron.David.Schneider@gmail.com>
2019-02-21 15:29:12 +0100

    Erste Versuche um Version von Paper Abel et al zu implementieren

--- agent_code/our_agent/callbacks.py
+++ agent_code/our_agent/callbacks.py
@@ -1,60 +1,32 @@
 
 import numpy as np
-from random import shuffle
 from time import time, sleep
-from collections import deque
 
 from settings import s
 
+import random
+from sklearn.cluster import KMeans
 
-def look_for_targets(free_space, start, targets, logger=None):
-    """Find direction of closest target that can be reached via free tiles.
+def statereduction(self):
+    # Gather information about the game state
+    arena = self.game_state['arena']
+    x, y, _, bombs_left = self.game_state['self']
+    bombs = self.game_state['bombs']
+    bomb_xys = [(x,y) for (x,y,t) in bombs]
+    others = [(x,y) for (x,y,n,b) in self.game_state['others']]
+    coins = self.game_state['coins']
+    bomb_map = np.ones(arena.shape) * 5
+    for xb,yb,t in bombs:
+        for (i,j) in [(xb+h, yb) for h in range(-3,4)] + [(xb, yb+h) for h in range(-3,4)]:
+            if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
+                bomb_map[i,j] = min(bomb_map[i,j], t)
 
-    Performs a breadth-first search of the reachable free tiles until a target is encountered.
-    If no target can be reached, the path that takes the agent closest to any target is chosen.
+    # statereduction-function needs to be changed!!
+    state = (x,y) #simplest case
+    s_dim = arena.shape
 
-    Args:
-        free_space: Boolean numpy array. True for free tiles and False for obstacles.
-        start: the coordinate from which to begin the search.
-        targets: list or array holding the coordinates of all target tiles.
-        logger: optional logger object for debugging.
-    Returns:
-        coordinate of first step towards closest target or towards tile closest to any target.
-    """
-    if len(targets) == 0: return None
+    return state,s_dim
 
-    frontier = [start]
-    parent_dict = {start: start}
-    dist_so_far = {start: 0}
-    best = start
-    best_dist = np.sum(np.abs(np.subtract(targets, start)), axis=1).min()
-
-    while len(frontier) > 0:
-        current = frontier.pop(0)
-        # Find distance from current position to all targets, track closest
-        d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
-        if d + dist_so_far[current] <= best_dist:
-            best = current
-            best_dist = d + dist_so_far[current]
-        if d == 0:
-            # Found path to a target's exact position, mission accomplished!
-            best = current
-            break
-        # Add unexplored free neighboring tiles to the queue in a random order
-        x, y = current
-        neighbors = [(x,y) for (x,y) in [(x+1,y), (x-1,y), (x,y+1), (x,y-1)] if free_space[x,y]]
-        shuffle(neighbors)
-        for neighbor in neighbors:
-            if neighbor not in parent_dict:
-                frontier.append(neighbor)
-                parent_dict[neighbor] = current
-                dist_so_far[neighbor] = dist_so_far[current] + 1
-    if logger: logger.debug(f'Suitable target found at {best}')
-    # Determine the first step towards the best found target tile
-    current = best
-    while True:
-        if parent_dict[current] == start: return current
-        current = parent_dict[current]
 
 
 def setup(self):
@@ -67,12 +39,12 @@ def setup(self):
     file for debugging (see https://docs.python.org/3.7/library/logging.html).
     """
     self.logger.debug('Successfully entered setup code')
-    np.random.seed()
-    # Fixed length FIFO queues to avoid repeating the same actions
-    self.bomb_history = deque([], 5)
-    self.coordinate_history = deque([], 20)
-    # While this timer is positive, agent will not hunt/attack opponents
-    self.ignore_others_timer = 0
+    self.reduced_state,self.state_dim = self.statereduction()
+
+    self.q = np.zeros((*self.state_dim,6))
+    self.epsilon = 0.2 # Exploration, Exploitation tradeoff
+
+
 
 
 def act(self):
@@ -88,120 +60,21 @@ def act(self):
     in settings.py, execution is interrupted by the game and the current value
     of self.next_action will be used. The default value is 'WAIT'.
     """
-    self.logger.info('Picking action according to rule set')
 
-    # Gather information about the game state
-    arena = self.game_state['arena']
-    x, y, _, bombs_left = self.game_state['self']
-    bombs = self.game_state['bombs']
-    bomb_xys = [(x,y) for (x,y,t) in bombs]
-    others = [(x,y) for (x,y,n,b) in self.game_state['others']]
-    coins = self.game_state['coins']
-    bomb_map = np.ones(arena.shape) * 5
-    for xb,yb,t in bombs:
-        for (i,j) in [(xb+h, yb) for h in range(-3,4)] + [(xb, yb+h) for h in range(-3,4)]:
-            if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
-                bomb_map[i,j] = min(bomb_map[i,j], t)
+    # possible actions to be taken
+    possible_actions = ['RIGHT', 'LEFT', 'UP', 'DOWN', 'BOMB','WAIT']
 
-    # If agent has been in the same location three times recently, it's a loop
-    if self.coordinate_history.count((x,y)) > 2:
-        self.ignore_others_timer = 5
+    # needs to be adjusted!!!
+    valid_actions =    range(6)
+
+    # in case of exploration, choose random:
+    prob_actions = [.23, .23, .23, .23, .08, 0.]
+
+    # take decision based on exploaration and exploitation strategy
+    if (random.random() < self.epsilon):
+        self.next_action = np.random.choice(possible_actions[valid_actions], p=prob_actions[valid_actions])
     else:
-        self.ignore_others_timer -= 1
-    self.coordinate_history.append((x,y))
-
-    # Check which moves make sense at all
-    directions = [(x,y), (x+1,y), (x-1,y), (x,y+1), (x,y-1)]
-    valid_tiles, valid_actions = [], []
-    for d in directions:
-        if ((arena[d] == 0) and
-            (self.game_state['explosions'][d] <= 1) and
-            (bomb_map[d] > 0) and
-            (not d in others) and
-            (not d in bomb_xys)):
-            valid_tiles.append(d)
-    if (x-1,y) in valid_tiles: valid_actions.append('LEFT')
-    if (x+1,y) in valid_tiles: valid_actions.append('RIGHT')
-    if (x,y-1) in valid_tiles: valid_actions.append('UP')
-    if (x,y+1) in valid_tiles: valid_actions.append('DOWN')
-    if (x,y)   in valid_tiles: valid_actions.append('WAIT')
-    # Disallow the BOMB action if agent dropped a bomb in the same spot recently
-    if (bombs_left > 0) and (x,y) not in self.bomb_history: valid_actions.append('BOMB')
-    self.logger.debug(f'Valid actions: {valid_actions}')
-
-    # Collect basic action proposals in a queue
-    # Later on, the last added action that is also valid will be chosen
-    action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-    shuffle(action_ideas)
-
-    # Compile a list of 'targets' the agent should head towards
-    dead_ends = [(x,y) for x in range(1,16) for y in range(1,16) if (arena[x,y] == 0)
-                    and ([arena[x+1,y], arena[x-1,y], arena[x,y+1], arena[x,y-1]].count(0) == 1)]
-    crates = [(x,y) for x in range(1,16) for y in range(1,16) if (arena[x,y] == 1)]
-    targets = coins + dead_ends + crates
-    # Add other agents as targets if in hunting mode or no crates/coins left
-    if self.ignore_others_timer <= 0 or (len(crates) + len(coins) == 0):
-        targets.extend(others)
-
-    # Exclude targets that are currently occupied by a bomb
-    targets = [targets[i] for i in range(len(targets)) if targets[i] not in bomb_xys]
-
-    # Take a step towards the most immediately interesting target
-    free_space = arena == 0
-    if self.ignore_others_timer > 0:
-        for o in others:
-            free_space[o] = False
-    d = look_for_targets(free_space, (x,y), targets, self.logger)
-    if d == (x,y-1): action_ideas.append('UP')
-    if d == (x,y+1): action_ideas.append('DOWN')
-    if d == (x-1,y): action_ideas.append('LEFT')
-    if d == (x+1,y): action_ideas.append('RIGHT')
-    if d is None:
-        self.logger.debug('All targets gone, nothing to do anymore')
-        action_ideas.append('WAIT')
-
-    # Add proposal to drop a bomb if at dead end
-    if (x,y) in dead_ends:
-        action_ideas.append('BOMB')
-    # Add proposal to drop a bomb if touching an opponent
-    if len(others) > 0:
-        if (min(abs(xy[0] - x) + abs(xy[1] - y) for xy in others)) <= 1:
-            action_ideas.append('BOMB')
-    # Add proposal to drop a bomb if arrived at target and touching crate
-    if d == (x,y) and ([arena[x+1,y], arena[x-1,y], arena[x,y+1], arena[x,y-1]].count(1) > 0):
-        action_ideas.append('BOMB')
-
-    # Add proposal to run away from any nearby bomb about to blow
-    for xb,yb,t in bombs:
-        if (xb == x) and (abs(yb-y) < 4):
-            # Run away
-            if (yb > y): action_ideas.append('UP')
-            if (yb < y): action_ideas.append('DOWN')
-            # If possible, turn a corner
-            action_ideas.append('LEFT')
-            action_ideas.append('RIGHT')
-        if (yb == y) and (abs(xb-x) < 4):
-            # Run away
-            if (xb > x): action_ideas.append('LEFT')
-            if (xb < x): action_ideas.append('RIGHT')
-            # If possible, turn a corner
-            action_ideas.append('UP')
-            action_ideas.append('DOWN')
-    # Try random direction if directly on top of a bomb
-    for xb,yb,t in bombs:
-        if xb == x and yb == y:
-            action_ideas.extend(action_ideas[:4])
-
-    # Pick last action added to the proposals list that is also valid
-    while len(action_ideas) > 0:
-        a = action_ideas.pop()
-        if a in valid_actions:
-            self.next_action = a
-            break
-
-    # Keep track of chosen action for cycle detection
-    if self.next_action == 'BOMB':
-        self.bomb_history.append((x,y))
+        self.next_action = possible_actions[np.argmax(self.q[...,valid_actions],axis=-1)]
 
 
 def reward_update(self):
